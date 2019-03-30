cf9343d23ef8b46f274c4fa0c2f91a3762b2a0be
Anca Trandafir <48153640+ancaTrandafir@users.noreply.github.com>
2019-03-30 13:03:42 +0200

    Interfata desktop

--- src/Domain/MedicamentValidator.java
+++
@@ -1,9 +0,0 @@
-package Domain;
-
-public class MedicamentValidator implements IValidator<Medicament> {
-    public void validate (Medicament med) {
-        if (med.getPrice()<=0) {
-            throw new RuntimeException("Price should be greater then 0");
-        }
-    }
-}
